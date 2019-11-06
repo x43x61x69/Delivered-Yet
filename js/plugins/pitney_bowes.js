b001b7db1f9822a80772aaff751809ac8e8c81f0
Cai <privacy@one.vg>
2019-11-06 11:46:19 +0800

    Update pitney_bowes.js

--- js/plugins/pitney_bowes.js
+++ js/plugins/pitney_bowes.js
@@ -8,42 +8,21 @@ $.carrier_PBI = function(code)
   p.id = code;
 
   $.ajax({
-    url: 'https://parceltracking.pb.com/ptsapi/track-packages/' + code,
-    type: 'GET',
+    url: 'https://reqbin.com/api/v1/Requests',
+    type: 'POST',
+    data: JSON.stringify(
+      {
+        'json':
+        {
+          'method': 'GET',
+          'url': 'https://parceltracking.pb.com/ptsapi/track-packages/' + code
+        }
+      }
+    ),
     dataType: 'json',
     success: function(data)
     {
       console.log(data);
-      // const msg = data[0];
-      // const status = data[1];
-      // if (status['body']['result'] == 'success' &&
-      //     msg['body']['host_rs'] !== undefined &&
-      //     msg['body']['host_rs'] !== null)
-      // {
-      //   p.type = $.trim(msg['body']['host_rs']['MAILTYPE']);
-      //   const items = msg['body']['host_rs']['ITEM'];
-      //   for (var i = 0; i < items.length; i++)
-      //   {
-      //     // console.log("data: " + JSON.stringify(items[i]));
-      //     var s = new StatusObj();
-      //     s.date = $.GetDate(items[i]['DATIME'], 'YYYYMMDDHHmmss', 8);
-      //     s.location = $.trim(items[i]['BRHNC']);
-      //     s.description = $.trim(items[i]['STATUS']);
-      //     if (s.description == '投遞成功')
-      //     {
-      //       p.delivered = true;
-      //     }
-      //     p.status.push(s);
-      //   }
-      // }
-      // else
-      // {
-      //   p.error = true;
-      //   if (msg['body']['msgData'] !== undefined)
-      //   {
-      //     p.errorMsg = msg['body']['msgData'].replace('KEY|', '');
-      //   }
-      // }
     },
     error: function()
     {
